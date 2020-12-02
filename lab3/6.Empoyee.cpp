f25f11f0695ac748239e2dc9ebde73fd95113b9a
abhinav-TB <abhinavtb@gmail.com>
2020-12-02 21:42:12 +0530

    6th q

--- lab3/6.Empoyee.cpp
+++ lab3/6.Empoyee.cpp
@@ -1,21 +1,102 @@
-# include <iostream>
+#include <iostream>
 
 using namespace std;
 
-class employee{
-   protected:
-   string name;
-   int empno;
-   int pay_rate;
+class Employee
+{
+protected:
+    string name;
+    int emp_no, pay_rate;
 
-   employee(string name,int empno,int pay){
-       this->name=name;
-       this->empno=empno;
-       this->pay_rate=pay;
-   }
-
+public:
+    Employee() {}
+    Employee(string name, int emp_no)
+    {
+        this->name = name;
+        this->emp_no = emp_no;
+        this->pay_rate = 0;
+    }
+    virtual void pay()
+    {
+       
+    }
 };
-class manager:private employee{
+
+class Manager : Employee
+{
     bool is_salaried;
-    
+
+public:
+    Manager(string name, int emp_no, bool is_salaried)
+    {
+        this->name = name;
+        this->emp_no = emp_no;
+        this->is_salaried = is_salaried;
+        this->pay_rate = 0;
+    }
+    void display()
+    {
+        cout << "Manager details: " << endl;
+        cout << "Name: " << name << endl;
+        cout << "Employee number: " << emp_no << endl;
+        cout << "Pay rate: " << pay_rate;
+        if (is_salaried)
+        {
+            cout << " monthly" << endl;
+        }
+        else
+        {
+            cout << " hourly" << endl;
+        }
+    }
+    void pay(int salary)
+    {
+        if (is_salaried)
+        {
+            pay_rate = salary;
+        }
+        else
+        {
+            pay_rate = salary / (30 * 24); // hourly wage
+        }
+    }
 };
+
+class Supervisor : Employee
+{
+    string department;
+
+public:
+    Supervisor(string name, int emp_no, string department)
+    {
+        this->name = name;
+        this->emp_no = emp_no;
+        this->department = department;
+        this->pay_rate = 0;
+    }
+    void display()
+    {
+        cout << "Supervisor details: " << endl;
+        cout << "Department: " << department << endl;
+        cout << "Name: " << name << endl;
+        cout << "Employee number: " << emp_no << endl;
+        cout << "Pay rate: " << pay_rate << " monthly" << endl;
+    }
+    void pay(int salary)
+    {
+        pay_rate = salary;
+    }
+};
+
+int main()
+{
+    Manager m("ABC", 123, false);
+    Supervisor s("ABD", 123, "Engineering");
+    m.display();
+    s.display();
+    m.pay(720000);
+    s.pay(720000);
+    m.display();
+    s.display();
+    return 0;
+}
